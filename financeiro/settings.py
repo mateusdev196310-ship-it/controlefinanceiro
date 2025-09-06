836cb5ff8bb5a3cef0608b84cec1bb50e1ac509f
Mateus Souza <mateus.dev196310@gmail.com>
2025-09-06 13:32:12 -0300

    Fix: Simplificar logging config e corrigir static files para produção

--- financeiro/settings.py
+++ financeiro/settings.py
@@ -150,9 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS removido para produção - apenas arquivos estáticos dos apps
 
 # Whitenoise configuration for serving static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
